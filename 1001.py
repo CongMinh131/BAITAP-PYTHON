0aff296bc8acca436985386af2212cdb384acd6e
CongMinh131 <114354999+CongMinh131@users.noreply.github.com>
2022-09-26 00:05:15 +0700

    Add files via upload

--- 1001.py
+++ 1001.py
@@ -1,2 +1,15 @@
-ten=input()
-print("Hello",ten+'!')
+def kt(n):
+    if (n<2):
+        return 0;  
+    for i in range(2,int(n**0.5)+1):
+        if n % i == 0:
+            return 0
+    return 1
+
+
+
+n=int(input())
+if(kt(n)):
+    print("YES")
+else:
+    print("NO")
