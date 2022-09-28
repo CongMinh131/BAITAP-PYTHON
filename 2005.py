cba862f1196224dccf1cc5b93bc2226ff1314a67
CongMinh131 <114354999+CongMinh131@users.noreply.github.com>
2022-09-28 22:10:06 +0700

    Update 2005.py

--- 2005.py
+++ 2005.py
@@ -1,7 +1,10 @@
 n=int(input())
 s=input().split()
-t=0
-for i in range(0,len(s)-1,2):
-    if(s[i]!=s[i+1]):
-        t=t+1
+t=0;
+for i in range(0,len(s)-1):
+    for j in range(i+1,len(s)):
+        a=int(s[i])
+        b=int(s[j])
+        if a>b:
+            t=t+1
 print(t)
