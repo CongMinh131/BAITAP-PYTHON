a3dce3541607d23900ac7c4442eb6c63eaf43945
CongMinh131 <114354999+CongMinh131@users.noreply.github.com>
2022-09-28 22:17:32 +0700

    Update 2002.py

--- 2002.py
+++ 2002.py
@@ -1,17 +1,14 @@
 def fibo(n):
-    f1 = 1
-    f2 = 1
-    fn = 2
-    if (n == 0 or n == 1):
+    if(n<0):
+        return -1
+    if(n==0 or n==1):
         return n
     else:
-        for i in range(2, n):
-            f1 = f2
-            f2 = fn
-            fn = f1 + f2
-        return fn
+        return fibo(n-1)+fibo(n-2)
 t=int(input())
 while(t>0):
-    a,b=map(int,input().split())
+    s=input().split()
+    a=int(s[0])
+    b=int(s[len(s)-1])
     for i in range(a,b+1):
         print(fibo(i),end=" ")
